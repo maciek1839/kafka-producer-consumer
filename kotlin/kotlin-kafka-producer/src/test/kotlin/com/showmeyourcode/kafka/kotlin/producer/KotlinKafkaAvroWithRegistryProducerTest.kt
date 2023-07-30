ff3486ef0588061b64e63dd01efbecc19ba87832
Maciek <maciek1839@gmail.com>
2023-07-30 15:57:47 +1000

    test: improve code coverage

--- kotlin/kotlin-kafka-producer/src/test/kotlin/com/showmeyourcode/kafka/kotlin/producer/KotlinKafkaAvroWithRegistryProducerTest.kt
+++ kotlin/kotlin-kafka-producer/src/test/kotlin/com/showmeyourcode/kafka/kotlin/producer/KotlinKafkaAvroWithRegistryProducerTest.kt
@@ -1,7 +1,15 @@
 package com.showmeyourcode.kafka.kotlin.producer
 
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.TopicPartition
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.*
+import java.util.concurrent.CompletableFuture
+import kotlin.test.assertFailsWith
 
 class KotlinKafkaAvroWithRegistryProducerTest {
 
@@ -12,5 +20,40 @@ class KotlinKafkaAvroWithRegistryProducerTest {
                 .withNumberOfMessages(10L)
                 .build()
         Assertions.assertThat(producer.numberOfMessages).isEqualTo(10L)
+    }
+
+    @Test
+    fun shouldProduceKafkaMessagesWhenAvroFileConfigurationIsValid() {
+        val kafkaProducer = mock<Producer<Long, GenericRecord>> {
+            on { send(any()) } doReturn CompletableFuture.completedFuture(
+                RecordMetadata(
+                    TopicPartition("topic1", 0),
+                    1,
+                    1,
+                    1,
+                    Long.MIN_VALUE,
+                    1,
+                    1,
+                ),
+            )
+        }
+        KotlinKafkaAvroWithRegistryProducer(kafkaProducer, 2L).produce()
+
+        verify(kafkaProducer, times(2)).send(any())
+    }
+
+    @Test
+    fun shouldThrowExceptionWhenCannotPublishMessages() {
+        val producer = mock<KafkaProducer<Long, GenericRecord>> {
+            on { send(any()) } doThrow RuntimeException("Buum!")
+        }
+
+        val throwable = assertFailsWith<RuntimeException>(
+            block = {
+                KotlinKafkaAvroWithRegistryProducer(producer, 2L).produce()
+            },
+        )
+
+        Assertions.assertThat(throwable).hasMessage("Buum!")
     }
 }
